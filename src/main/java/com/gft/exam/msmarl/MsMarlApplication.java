fdb529a02a266937ca3894cf888819c602de7a7b
mramon <miguel.ramon.leon@gmail.com>
2019-01-27 12:53:14 -0600

    init version

---
+++ src/main/java/com/gft/exam/msmarl/MsMarlApplication.java
@@ -0,0 +1,14 @@
+package com.gft.exam.msmarl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMarlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMarlApplication.class, args);
+	}
+
+}
+
